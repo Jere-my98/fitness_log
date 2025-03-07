a2cd4b58ee0e93575d46bf65902ad6c904f27661
Jeremy Nyangi <justinjerome401@gmail.com>
2025-03-07 16:51:30 +0300

    chore: add User Profile Form

---
+++ userprofile/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import UserProfile
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['name','age','avatar','mantra']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control'}),
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'mantra': forms.Textarea(attrs={'class': 'form-control'})
+        }
